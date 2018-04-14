1260a1adcbd85c5e8858d24789ea0ee39827cba6
Melissa Franklin <trebleclef777@gmail.com>
2018-04-14 13:49:07 -0500

    commit

--- main.py
+++ main.py
@@ -7,3 +7,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://
 build-a-blog:buildtheblog@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
+Class Blog(db.Model)
+
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(120))
+        body = db.Column(db.String(900))
+
+
+
+
+
+@app.route('/', methods=['Post', 'GET'])
+def index():
+
+
+
+
+
+
+@app.route('blog', methods=['POST'])
+def Blog():
+
+
+
+
+@app.route('newpost', methods=['POST'])
+def newpost():
